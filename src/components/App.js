3c316f41d34a9c34ec7842456668bd79330a5632
d-otis <dan@dan-foley.com>
2020-09-26 01:29:58 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -59,6 +59,7 @@ class App extends React.Component {
     // making a copy of the pets array, 
     // and changing the one member whose ID matches the id argument
     // all other pet members remain the same
+    
     // const pets = this.state.pets.map(p => {
     //   return p.id === petId ? { ...p, isAdopted: true } : p;
     // });
